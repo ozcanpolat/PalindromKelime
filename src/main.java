35e9e5e58ddbe92c65fa2d086a123d10d6c01a62
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-03-30 02:37:54 +0300

    PalindromKelime

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static boolean isPalindrome(String str){
+        String reverse= "" ;
+        for(int i=str.length()-1 ; i >=0 ; i--){
+            reverse+=str.charAt(i);
+        }
+        if(str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+
+    }
+}
